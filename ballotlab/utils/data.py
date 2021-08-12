10b74a05adfe55d058b60d4ba184da3a5758fbbf
Neil Johnson <neil@cadent.com>
2021-08-12 11:50:37 -0400

    Read XML and JSON election files to Python objects

--- ballotlab/utils/data.py
+++ ballotlab/utils/data.py
@@ -3,32 +3,61 @@
 
 from files import FileTools
 from lxml import objectify
+import json
+
+supported_ext_types = [".json", ".JSON", ".xml", ".XML"]
+# create a string of supported extensions from list
+ext_types_str = " ".join(str(item) for item in supported_ext_types)
 
 
 class ElectionData:
     def __init__(self, data_file, data_dir):
+
+        election_file = FileTools(data_file, data_dir)
+        if not election_file.file_found:
+            msg = "Election data file {} not found in directory {}"
+            raise RuntimeError(msg.format(str(data_file, data_dir)))
+
         self.data_file = data_file
         self.data_dir = data_dir
-        self.abs_path_to_data = self.find_data_file(self.data_file, self.data_dir)
-        if self.abs_path_to_data:
-            self.xml = self.parse_xml(self.abs_path_to_data)
+        self.abs_path_to_data = election_file.abs_path_to_file
+        self.ext = election_file.ext
 
-    def find_data_file(self, find_file, search_dir):
-        dataFile = FileTools(find_file, search_dir)
-        if dataFile.file_found:
-            return dataFile.abs_path_to_file
-        else:
-            return ""
+        if self.ext not in supported_ext_types:
+            msg = "Election data must be one of the following file types: " "{}. Got {}"
+            raise RuntimeError(msg.format(ext_types_str, self.ext))
+
+        if self.ext in [".xml", ".XML"]:
+            self.election_data = self.parse_xml(self.abs_path_to_data)
+        elif self.ext in [".json", ".JSON"]:
+            self.election_data = self.parse_json(self.abs_path_to_data)
 
     def parse_xml(self, xml_file):
-        """"""
+        """
+        parse xml file into Python objects
+        """
         with open(xml_file) as f:
             xml = f.read()
         return objectify.fromstring(xml)
+        # was dump(xml_election.xml)
+
+    def parse_json(self, json_file):
+        """
+        parse json file into Python objects
+        """
+        # read file
+        with open(json_file, "r") as jsf:
+            json_data = json.load(jsf)
+        return json_data
 
 
 if __name__ == "__main__":
-    election = ElectionData("nist_sample_election_report.xml", "assets/data")
-    print(election.data_file)
-    print(election.abs_path_to_data)
-    print(objectify.dump(election.xml))
+    xml_election = ElectionData("nist_sample_election_report.xml", "assets/data")
+    print(xml_election.data_file)
+    print(xml_election.abs_path_to_data)
+    print(xml_election.election_data)
+
+    json_election = ElectionData("BallotStudio_16_Edits.JSON", "assets/data/")
+    print(json_election.data_file)
+    print(json_election.abs_path_to_data)
+    print(json_election.election_data)
--- ballotlab/utils/files.py
+++ ballotlab/utils/files.py
@@ -8,6 +8,7 @@ class FileTools:
     def __init__(self, file_name="", rel_path=""):
         self.file_name = file_name
         self.rel_path = rel_path
+        self.ext = ""
         self.file_found = False
         self.package_name = "BallotLab"
         # get the absolute path to this module
@@ -17,19 +18,20 @@ class FileTools:
             raise Exception("Not executing in the expected package.")
         # if this module is part of the specified package, build the absolute path
         self.package_root = os.path.join(self.code_dir[:base_len], self.package_name)
-        # check for the existence of
         self.full_path = self.package_root
         # add a relative path to the target directory
         if self.rel_path:
             self.full_path = os.path.join(self.full_path, self.rel_path)
         if self.file_name:
             self.abs_path_to_file = os.path.join(self.full_path, self.file_name)
+            # if the file exists, get the extension and set the file found flag to True
             if isfile(self.abs_path_to_file):
+                self.ext = os.path.splitext(self.file_name)[1]
                 self.file_found = True
 
 
 if __name__ == "__main__":
-    print("Default settins:")
+    print("Default settings:")
     file_defaults = FileTools()
     print(file_defaults.code_dir)
     print(file_defaults.package_root)

54d92ea6fac046eee96d6cf9098eff667d64f747
Neil Johnson <neil@cadent.com>
2021-08-30 15:18:25 -0400

    Read all Election data items (no lists)

--- src/ballotlab/data.py
+++ src/ballotlab/data.py
@@ -54,10 +54,12 @@ class ElectionData:
 
         # self.text_rpt = self.print_line("- ", 40)
         # Election contains BallotStyle, Candidate and Contest.
-        rpt_title = "Election Report\n"
-        self.text_rpt = rpt_title
+        self.text_rpt = "EDF name: {}\n".format(self.data_file)
+        self.text_rpt += "Location: {}\n".format(self.abs_path_to_data)
+        # rpt_title = "Election Report\n"
+        # self.text_rpt += rpt_title
         # self.text_rpt(self.print_line("=", len(rpt_title)))
-        self.text_rpt += "File: {}\n".format(self.data_file)
+        # self.text_rpt += "File: {}\n".format(self.data_file)
         # self.text_rpt += "Object election_rpt is {}.\n".format(type(self.election_rpt))
         # pprint.pprint(self.election_rpt)
         self.text_rpt += "Election name: {}\n".format(self.elect_name)
@@ -97,11 +99,11 @@ if __name__ == "__main__":
     # print(xml_election.election_rpt)
 
     json_election = ElectionData("NIST_sample.json", "assets/data/")
-    print(json_election.data_file)
-    print(json_election.abs_path_to_data)
+    # print(json_election.data_file)
+    # print(json_election.abs_path_to_data)
     # print JSON dict
     # print(json_election.election_rpt)
 
     json_election = ElectionData("BallotStudio_16_Edits.JSON", "assets/data/")
-    print(json_election.data_file)
-    print(json_election.abs_path_to_data)
+    # print(json_election.data_file)
+    # print(json_election.abs_path_to_data)
